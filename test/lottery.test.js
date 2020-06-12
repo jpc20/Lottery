76c15ff18a5c2ff49f2bccb74f5afe0bfe047dc3
Jack Cullen <jpcullen20@gmail.com>
2020-06-12 09:44:33 -0600

    Test the contract from start to finish

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -67,6 +67,7 @@ describe('Lottery Contract', () => {
     }
     assert(false);
   });
+
   it('only manager can pick winner', async () => {
     try {
       await lottery.methods.enter().send({
@@ -81,5 +82,23 @@ describe('Lottery Contract', () => {
       return;
     }
     assert(false);
+  });
+
+  it('sends eth to winner and resets players', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    const players = await lottery.methods.getPlayers().call({ from: accounts[0] });
+    const lotteryBalance = await web3.eth.getBalance(lottery.options.address);
+    
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+    assert.equal(0, players.length)
+    assert.equal(0, lotteryBalance)
   });
 });
